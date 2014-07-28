2cc2d60712ee26b68281af3d9b603a9a495d3e3a
Ildar Gumerov <superlight89@yandex.ru>
2014-07-27 23:17:54 -0700

    stuff3

--- iUniversity/junk/core/u_type.py
+++ iUniversity/junk/core/u_type.py
@@ -7,26 +7,28 @@ class UType(object):
     _db_table_name = UField.Virtual()
 
     @classmethod
-    def _get_all_attributes(cls):
+    def _get_all_fields(cls):
+        result = dict()
         for pcls in cls.__mro__:
             if pcls is object:
                 continue
             for attr_name, attr in pcls.__dict__.iteritems():
                 if not isinstance(attr, UField):
                     continue
-                yield attr_name, attr
+                result[attr_name] = attr
+        return result
 
     @classmethod
     def get_const_attributes(cls):
         result = dict()
-        for attr_name, attr in cls._get_all_attributes():
+        for attr_name, attr in cls._get_all_fields().iteritems():
             if attr.is_constant():
                 result[attr_name] = attr.get_const_value()
         return result
 
     @classmethod
     def get_db_table_name(cls):
-        return cls._db_table_name.get_value()
+        return cls._db_table_name.get_const_value()
 
     """@classmethod
     def get_data_template(cls):
@@ -36,13 +38,24 @@ class UType(object):
             result[key] = value_type()
         return result"""
 
-    def validate(self):
-        for attr_name, attr in self._get_all_attributes():
-            attr.validate()
+    @classmethod
+    def validate(cls):
+        for attr_name, attr in cls._get_all_fields().iteritems():
+            print attr_name, attr
+            if attr.is_virtual():
+                raise BaseException("Found virtual field: %s" % attr_name)
+
+    @classmethod
+    def check_value(cls, **kwargs):
+        attrs = cls._get_all_fields()
+        for attr_name, attr_value in kwargs.iteritems():
+            if attr_name not in attrs:
+                raise BaseException("Unknown attribute: %s" % attr_name)
+            attrs[attr_name].check_value(attr_value)
 
 
 class UVertexType(UType):
-    UType._db_table_name = UField.Constant('u_vertices')
+    _db_table_name = UField.Constant('u_vertices') # HOW TO REWRITE THIS?
 
     uid = UField.RequiredInteger()
     utype = UField.RequiredShortInteger()
@@ -50,7 +63,7 @@ class UVertexType(UType):
 
 
 class UEdgeType(UType):
-    UType._db_table_name = UField.Constant('u_edges')
+    _db_table_name = UField.Constant('u_edges')
 
     uid1_type = UField.Virtual()
     uid2_type = UField.Virtual()
@@ -64,6 +77,7 @@ class UEdgeType(UType):
 
 
 if __name__ == '__main__':
+    UVertexType.check_value(uid=1, utype=2)
     UVertexType().validate()
     try:
         UEdgeType().validate()
